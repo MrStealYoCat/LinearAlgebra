7c7eba66d4058854aedbe85de935376285826737
alanhumphreyiv <alanghumphreyiv@gmail.com>
2022-11-16 18:24:34 -0800

    Changed key functionality to test transformations of scaling.

--- src/com/mrstealyocat/controlListeners/KeyListener.java
+++ src/com/mrstealyocat/controlListeners/KeyListener.java
@@ -22,11 +22,14 @@ public class KeyListener {
 	public static void keyCallback(long window, int key, int scancode, int action, int mods) {
 		if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
 			glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
-		if (key == GLFW_KEY_ENTER && action == GLFW_PRESS) {
-			shape.transform();
+		if (key == GLFW_KEY_EQUAL && action == GLFW_PRESS) {
+			shape.transform(new float[]{1f,1f,1.1f});
+		}
+		if (key == GLFW_KEY_MINUS && action == GLFW_PRESS) {
+			shape.transform(new float[]{1f,1f,0.9f});
 		}
 		if (key == GLFW_KEY_R && action == GLFW_PRESS) {
-			shape.rotate(90);
+			shape.rotate();
 		}
 		if (action == GLFW_PRESS) {
 			get().keyPressed[key] = true;
