758d1f9d0c9cb8a50d20d99a4cc6a10e9a804539
alanhumphreyiv <alanghumphreyiv@gmail.com>
2022-11-28 17:58:14 -0800

    hors.jpg

--- src/com/mrstealyocat/Shapes/Shape.java
+++ src/com/mrstealyocat/Shapes/Shape.java
@@ -1,11 +1,9 @@
 package com.mrstealyocat.Shapes;
 
-import com.mrstealyocat.controlListeners.MouseListener;
+import com.mrstealyocat.TextIO;
 
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
-import static java.lang.Math.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Shape {
 
@@ -15,18 +13,42 @@ public class Shape {
 	//private Matrix transformationMatrix;
 
 	public Shape() {
-		this.vertices = new Matrix[]{
-//						new Matrix(0.2f, 0.1f),
-//						new Matrix(0.1f, 0.4f),
-//						new Matrix(0.3f, 0.7f)
-							new Matrix(-0.3f, 0.1f),
-							new Matrix(-0.3f, -0.3f),
-							new Matrix(0.3f, -0.3f),
-							new Matrix(0.3f, 0.1f),
-							new Matrix(-0.3f, 0.1f),
-							new Matrix(0f, 0.4f),
-							new Matrix(0.3f, 0.1f)
-		};
+//		this.vertices = new Matrix[]{
+//							new Matrix(-0.3f, 0.1f),
+//							new Matrix(-0.3f, -0.3f),
+//							new Matrix(0.3f, -0.3f),
+//							new Matrix(0.3f, 0.1f),
+//							new Matrix(-0.3f, 0.1f),
+//							new Matrix(0f, 0.4f),
+//							new Matrix(0.3f, 0.1f)
+//		};
+		this.vertices = fileInput("src/com/mrstealyocat/Shapes/vertices.txt");
+	}
+
+	public Matrix[] fileInput(String filename) {
+		TextIO.readFile(filename);
+		List<float[]> verts = new ArrayList<>();
+		String[] parts;
+		while (!TextIO.eof()) {
+			parts = TextIO.getln().split(" ");
+			verts.add(new float[]{Float.parseFloat(parts[0]), Float.parseFloat(parts[1])});
+		}
+		Matrix[] result = new Matrix[verts.size()];
+
+		float biggestNum = 0f;
+		for (float[] vert : verts) {
+			if (vert[0] > biggestNum)
+				biggestNum = vert[0];
+			if (vert[1] > biggestNum)
+				biggestNum = vert[1];
+		}
+		biggestNum = biggestNum * 2;
+
+		for (int i=0; i< verts.size();i++) {
+			verts.set(i, new float[]{verts.get(i)[0]/biggestNum, verts.get(i)[1]/biggestNum});
+			result[i] = new Matrix(verts.get(i)[0], verts.get(i)[1]);
+		}
+		return result;
 	}
 
 	public Matrix[] getVertices() {
